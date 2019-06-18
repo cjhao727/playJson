57e044f535a8352df2ef9bc83fdd7284e562ea8d
Jay Cui <cj.hao727@gmail.com>
2019-06-18 14:06:04 -0700

    Initial commit

---
+++ src/main/java/com/example/j/playJson/PlayJsonApplication.java
@@ -0,0 +1,13 @@
+package com.example.j.playJson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayJsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayJsonApplication.class, args);
+	}
+
+}
